eecf0d4235113f799270a1cf53e6da1efc963f4d
icpp <icpp@icpp.world>
2023-04-29 12:05:35 -0400

    use `icpp-pro` name

--- src/icpp/pro.py
+++ src/icpp/pro.py
@@ -1,5 +1,5 @@
 """
-Handles licensing of icpp_pro
+Handles licensing of icpp-pro
 
 ********************************************************************************
 * IT IS NOT ALLOWED TO MODIFY THIS FILE OR ANY FILE THAT CALLS THESE FUNCTIONS *
@@ -14,11 +14,12 @@ def exit_if_not_pro() -> None:
     """Exit if not running a licensed icpp-pro."""
     if not is_pro():
         typer.echo("--")
-        typer.echo("ERROR: this command requires icpp_pro")
+        typer.echo("ERROR: this command requires icpp-pro")
+        typer.echo(" ")
         typer.echo("For licensing, contact us at icpp@icpp.world")
         sys.exit(1)
 
 
 def is_pro() -> bool:
-    """Returns true if running a licensed version of icpp_pro."""
+    """Returns true if running a licensed version of icpp-pro."""
     return True

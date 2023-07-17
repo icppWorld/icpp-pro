a3805072540428cfc5b6e9eb1baddfb10ec7ac2b
icpp <icpp@icpp.world>
2023-07-17 15:53:20 -0400

    3.0.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="C++ Canister Development Kit (CDK) for the Internet Computer",  # Optional
+    description="The `lite` version of icpp-pro",  # Optional
     #
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.

876f9595b58bb484f6d9365c5c1e0e89a0d1cea3
icpp <icpp@icpp.world>
2023-08-07 13:48:51 -0400

    Add include files for (de)serialization

--- src/icpp/ic/ic0mock/mock_ic.h
+++ src/icpp/ic/ic0mock/mock_ic.h
@@ -1,6 +1,8 @@
 // To be included in the `native/main.cpp` of icpp projects.
 #pragma once
 
+#include "candid_deserialize.h"
+#include "candid_serialize.h"
 #include "candid_type.h"
 #include "candid_type_all_includes.h"
 #include "global.h"

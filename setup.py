bc4057a9ec219b8e3e5ac3634650214d68a4f7e7
icpp <icpp@icpp.world>
2025-01-17 09:07:37 -0500

    5.0.1

--- setup.py
+++ setup.py
@@ -40,16 +40,6 @@ setup(
     #
     packages=find_packages(where="src"),  # Required
     #
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        f"icpp-candid=={version.__version__}"
-    ],  # Optional
-    #
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     # https://setuptools.pypa.io/en/latest/userguide/datafiles.html#package-data
--- src/icpp/version.py
+++ src/icpp/version.py
@@ -10,4 +10,4 @@ see:
 https://packaging.python.org/guides/single-sourcing-package-version/
 """
 
-__version__ = "5.0.0"
+__version__ = "5.0.1"
